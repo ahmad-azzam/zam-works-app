9c668e0a88c001edc964bb54a8095e779c685393
AhmadAzzam <ahmad.azzam@inticorp.tech>
2024-03-05 13:51:44 +0700

    feat(footer): create footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-primary-300 py-5">
+      <div className="container mx-auto">
+        <span className="text-white">
+          &copy; Copyright {new Date().getFullYear()} zam-works
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
